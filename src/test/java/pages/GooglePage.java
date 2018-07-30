3eb15bb1213e73d664384436f890e6ff52b9e3fb
Artem <somemail2344@gmail.com>
2018-07-30 18:34:13 +0300

    Added selenium and selenide tests

--- src/test/java/pages/GooglePage.java
+++ src/test/java/pages/GooglePage.java
@@ -1,4 +1,34 @@
 package pages;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import static com.codeborne.selenide.Selenide.*;
 
 public class GooglePage {
+
+    public WebDriver driver;
+
+    public GooglePage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public void fieldRamblerSelenide(String word){
+        $(By.id("search_form_input_homepage")).sendKeys(word);
+    }
+
+    public void buttonSearchSelenide(){
+        $(By.id("search_button_homepage")).click();
+    }
+//////////////////////////////////// - selenium selectors
+
+
+    public void fieldRambler(String word){
+        driver.findElement(By.id("search_button_homepage")).click();
+        driver.findElement(By.id("search_button_homepage")).sendKeys(word);
+    }
+
+    public void buttonSearch(){
+        driver.findElement(By.id("search_button_homepage")).click();
+    }
+
 }
--- src/test/java/tests/MainTests.java
+++ src/test/java/tests/MainTests.java
@@ -1,4 +1,50 @@
 package tests;
 
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import pages.GooglePage;
+
+import java.util.concurrent.TimeUnit;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class MainTests {
+
+    public GooglePage googlePage;
+
+    @Test
+    public void userCanSearchAnyKeyword() {
+
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver");
+        Configuration.browser = "chrome";
+
+        open("http://google.com");
+        $(By.name("q")).val("selenide").pressEnter();
+        $$("#ires .g").shouldHave(CollectionCondition.size(10));
+        $("#ires .g").shouldHave(Condition.text("selenide.org"));
+    }
+
+    @Test
+    public void onlyWebdriver(){
+        WebDriver driver;
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        googlePage = new GooglePage(driver);
+        driver.get("https://duckduckgo.com/");
+        //googlePage.fieldRamblerSelenide("selenium");
+        //googlePage.buttonSearchSelenide();
+        googlePage.fieldRambler("ллллэээ     ");
+        googlePage.buttonSearch();
+
+    }
+
 }
