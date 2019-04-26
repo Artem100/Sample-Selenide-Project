aa6d5f6758689a5fd3fe47a0ca447f65d707a744
Artem <somemail2344@gmail.com>
2019-04-26 16:02:36 +0300

    added test with lists

--- src/test/java/tests/TestSelenide.java
+++ src/test/java/tests/TestSelenide.java
@@ -1,11 +1,15 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 import pages.LoginBySelenide;
+
+import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestSelenide {
@@ -18,5 +22,13 @@ public class TestSelenide {
         loginBySelenide.loginField("");
         loginBySelenide.passwordField("");
         loginBySelenide.loginButton();
+    }
+
+    @Test
+    public void googleSearch() {
+        open("http://google.com");
+        $(By.name("q")).val("selenide").pressEnter();
+        $$("#ires .g").shouldHave(size(10));
+        $("#ires .g").shouldHave(text("selenide.org"));
     }
 }
